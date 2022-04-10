b7a7a1b474261fceaaf1875a368850ce837bc207
imhousin <tomwhittingtont@gmail.com>
2022-04-10 18:52:30 +0100

    "Added first plotting function"

--- EDA.py
+++ EDA.py
@@ -5,6 +5,7 @@ from rdkit.Chem import Descriptors, Lipinski
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 pd.set_option('display.max_columns', 8)
 
@@ -21,11 +22,13 @@ def generate_descriptors(input_df):
 
 def pIC50(input_df):
     """Caps IC50 at 100000000 before converting to pIC50 to increase uniformity"""
-    input_df[input_df["standard_value"] > 100000000] = 100000000
+    input_df["standard_value"][input_df["standard_value"] > 100000000] = 100000000
     input_df["pIC50"] = -np.log10(df["standard_value"]*(10**-9))
     input_df.drop(columns='standard_value', inplace=True)
+    df.to_csv()
     return input_df
 
+condition = 'coronavirus'
 
 df = pd.read_csv('CSV/coronavirus_CHEMBL3927_preprocessed.csv')
 
@@ -33,4 +36,10 @@ generate_descriptors(df)
 
 pIC50(df)
 
-print(df)
+df = df[df["act_class"] != 'intermediate']
+
+sns.countplot(x="act_class", data=df, edgecolor="black")
+plt.xlabel('Bioactivity class', fontsize=14, fontweight='bold')
+plt.ylabel('Frequency', fontsize=14, fontweight='bold')
+plt.savefig(condition + ' class frequency plot.svg')
+plt.show()
--- Import_Preprocess.py
+++ Import_Preprocess.py
@@ -3,47 +3,50 @@ from chembl_webresource_client.new_client import new_client
 import tk
 import easygui
 
+def classifier(x):
+    """Classifies molecule activity into active, intermediate and inactive vis IC50 value"""
+    if x >= 10000:
+        return "inactive"
+    elif x <= 1000:
+        return "active"
+    else:
+        return "intermediate"
 
-# Input box will likely be used in future
+
+# TODO: Add input box to allow selecting conditons
 
 # condition = easygui.enterbox('What condition are you interested in')
 
 # Target is selected and searched within the Chembl db before being turned into a dataframe from dict
 
-condition='coronavirus'
+condition = 'coronavirus'
 target = new_client.target
-target_query=target.search(condition)
-targets=pd.DataFrame.from_dict(target_query)
+target_query = target.search(condition)
+targets = pd.DataFrame.from_dict(target_query)
 
 # Target row is subsetted from dataframe and it's chembl id is extracted. Any target can be chosen and searched
 
-selected_target=targets["target_chembl_id"][4]
+# TODO: Add way of choosing target and saving target name
 
-# Now using the target chembl id the database is searched for activity data but only returning results that have activity using IC50.
+selected_target = targets["target_chembl_id"][4]
 
-activity=new_client.activity
-act_query=activity.filter(target_chembl_id=selected_target).filter(standard_type="IC50")
-df=pd.DataFrame.from_dict(act_query)
+# Now using the target chembl id the database is searched for activity data but only returning results that have
+# activity using IC50.
 
+activity = new_client.activity
+act_query = activity.filter(target_chembl_id=selected_target).filter(standard_type="IC50")
+df = pd.DataFrame.from_dict(act_query)
 
-df.to_csv(condition + '_' + selected_target + '_' + 'query_raw.csv', index=False)
+# df.to_csv(condition + '_' + selected_target + '_' + 'query_raw.csv', index=False)
 
-df=df[["canonical_smiles", "molecule_chembl_id", "standard_value"]]
+# Unneeded columns and NAs are dropped and standard_value is renamed to IC50
+df = df[["canonical_smiles", "molecule_chembl_id", "standard_value"]]
+df.rename(columns={'standard_value' : 'IC50'}, inplace=True)
+df = df[df["IC50"].notna()]
+df["IC50"] = df["IC50"].astype("float")
 
-df=df[df["standard_value"].notna()]
+# Molecules are classified via IC50
+df["act_class"] = df["IC50"].apply(lambda x: classifier(x))
 
-df["standard_value"] = df["standard_value"].astype("float")
-
-act_class = []
-for i in df["standard_value"]:
-    if float(i) >=10000:
-        act_class.append("inactive")
-    elif float(i) <= 1000:
-        act_class.append("active")
-    else:
-        act_class.append("intermediate")
-
-df["act_class"]=act_class
-
-df.to_csv(condition + '_' + selected_target + '_' + 'preprocessed.csv', index=False)
 print(df)
+# df.to_csv(condition + '_' + selected_target + '_' + 'preprocessed.csv', index=False)
